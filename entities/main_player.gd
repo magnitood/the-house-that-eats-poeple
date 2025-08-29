38b10a8b163a8c7e83688260994e4e24a43583ea
Parthasarathi <theeparthasarathi@gmail.com>
2025-08-30 00:28:42 +0530

    player: physics done with state machine

--- entities/main_player.gd
+++ entities/main_player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-enum State {IDLE, RUNNING, HIDING, INTERACTING}
+enum State {IDLE, RUNNING, HIDING, INTERACTING, TRAPPED}
 
-var acceleration: float
-var deceleration: float
-var top_speed: int
+var acceleration: float = 650
+var deceleration: float = 800
+var top_speed: int = 112
+
 var player_state: State
 
 func _ready():
@@ -12,14 +13,33 @@ func _ready():
 	velocity = Vector2(0, 0)
 
 func _physics_process(delta):
+	print(player_state)
+	
 	match (player_state):
 		State.IDLE:
-			if Input.is_action_just_pressed("left"):
-				player_state = State.RUNNING
-				velocity.x += delta * acceleration
-			elif Input.is_action_just_pressed("right"):
+			if Input.is_action_pressed("left"):
 				velocity.x -= delta * acceleration
+				player_state = State.RUNNING
+			elif Input.is_action_pressed("right"):
+				velocity.x += delta * acceleration
+				player_state = State.RUNNING
 
 		State.RUNNING:
-			var direction: int
+			if Input.is_action_pressed("left"):
+				velocity.x -= delta * acceleration
+			elif Input.is_action_pressed("right"):
+				velocity.x += delta * acceleration
+			else: # decelerating
+				if velocity.x > 0:
+					velocity.x -= delta * deceleration
+					velocity.x = clampf(velocity.x, 0, top_speed)
+				else:
+					velocity.x += delta * deceleration
+					velocity.x = clampf(velocity.x, -top_speed, 0)
+
+			velocity.x = clampf(velocity.x, -top_speed, top_speed)
+			if (velocity.x == 0):
+				player_state = State.IDLE
+		_:
+			pass
 	move_and_slide()
