9711a0a8bde611361bcbc428812104e8e3959e96
sayantam-123 <sayantammaji@gmail.com>
2025-08-30 20:17:35 +0530

    starting things done

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -4,18 +4,20 @@ var time:float = 0
 var max_pos:Vector2
 var temp:Array
 var max:float
-const BHOOT = preload("res://scenes/bhoot.tscn")
+const BHOOT = preload("res://entities/bhoot.tscn")
 var has_ghost:bool = false
+var can_start:bool = false
 
 
 func _ready() -> void:
-	max_pos = get_tree().get_first_node_in_group("spawn").global_position
-	max = absf(get_tree().get_first_node_in_group("spawn").global_position.x - get_tree().get_first_node_in_group("player").global_position.x)
+	if get_tree().has_group("spawn"):
+		max_pos = get_tree().get_first_node_in_group("spawn").global_position
+		max = absf(get_tree().get_first_node_in_group("spawn").global_position.x - get_tree().get_first_node_in_group("player").global_position.x)
 
 func _process(delta: float) -> void:
 	time+=delta
 	#print(time)
-	if has_ghost == false:
+	if has_ghost == false and can_start == true:
 		#check_spawn()
 		pass
 	
@@ -34,3 +36,10 @@ func spawn():
 	var thongle = BHOOT.instantiate()
 	thongle.global_position = max_pos
 	add_child(thongle)
+
+func level_start():
+	time = 0
+	can_start = true
+	if get_tree().has_group("spawn"):
+		max_pos = get_tree().get_first_node_in_group("spawn").global_position
+		max = absf(get_tree().get_first_node_in_group("spawn").global_position.x - get_tree().get_first_node_in_group("player").global_position.x)
